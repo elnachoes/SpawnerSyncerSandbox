e4290042f098e41d94624e3f874ca8dd3528438e
Corbin Martin <corbin.martin@protonmail.com>
2023-08-20 16:45:31 -0700

    the server can now host as a player but the architecture is still server authoritive

--- Player.gd
+++ Player.gd
@@ -4,15 +4,22 @@ const SPEED = 300.0
 
 var assigned_net_id = 0
 
+func is_controlled_by_server() -> bool : return multiplayer.is_server() and assigned_net_id == 1
+
 func _physics_process(_delta):
 	# if the player is a client send the input to server and it will handle movement
 	# the changing position will come back through the synchronizer
-	if not multiplayer.is_server():
-		handle_input.rpc_id(1, Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down")))
-	move_and_slide()
+	var input_direction = Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down"))
+	
+	if is_controlled_by_server():
+		handle_input(input_direction)
+	
+	if multiplayer.is_server():
+		move_and_slide()
+	else:
+		handle_input.rpc_id(1, input_direction)
 
 @rpc("any_peer", "call_remote", "unreliable")
-func handle_input(dierection : Vector2):  
-	# when the client sends input 
-	if assigned_net_id == multiplayer.get_remote_sender_id():
-		velocity = dierection.normalized() * SPEED
+func handle_input(input_direction : Vector2):  
+	if multiplayer.get_remote_sender_id() == assigned_net_id or is_controlled_by_server():
+		velocity = input_direction.normalized() * SPEED
--- game.gd
+++ game.gd
@@ -4,10 +4,10 @@ extends Node
 @onready var player_spawn_location = $"PlayerSpawnLocation"
 @onready var player_spawn = $"PlayerSpawn"
 @onready var net_id_label = $"ConnectionMenu/HBoxContainer/NetIdLabel"
+@onready var ip_address_input = $"ConnectionMenu/HBoxContainer/VBoxContainer/IpAddress"
+@onready var port_input = $"ConnectionMenu/HBoxContainer/VBoxContainer/Port"
 
-const PORT = 9009
-const IP_ADDRESS = "127.0.0.1"
-const MAX_CLIENTS = 3
+const MAX_CLIENTS = 4
 
 var player_scene = load("res://player.tscn")
 
@@ -20,32 +20,41 @@ func _ready():
 # setup a server and the nessesary events and set the label text
 func create_server():
 	var peer = ENetMultiplayerPeer.new()
-	peer.create_server(PORT, MAX_CLIENTS)
+	peer.create_server(int(port_input.text), MAX_CLIENTS)
 	multiplayer.multiplayer_peer = peer
 	multiplayer.peer_connected.connect(handle_client_connected)
 	multiplayer.peer_disconnected.connect(handle_client_disconnected)
 	net_id_label.text = "Server"
 
-func _on_host_button_pressed():
-	create_server();
-
-# create client and set label text
-func _on_connect_button_pressed():
-	var peer = ENetMultiplayerPeer.new()
-	peer.create_client(IP_ADDRESS, PORT)
-	multiplayer.multiplayer_peer = peer
-	net_id_label.text = "Client Net Id : %s" % multiplayer.get_unique_id()
-
-# whenever a client connects spawn a new player
-func handle_client_connected(id : int):
+func create_player(id : int):
 	var player = player_scene.instantiate()
 	players.append(player)
 	player.name = "player_%s" % id
 	player.assigned_net_id = id
 	player.position = player_spawn_location.position
 	player_spawn.call_deferred("add_child", player)
-	
+
+func _on_host_button_pressed():
+	create_server()
+
+# create client and set label text
+func _on_connect_button_pressed():
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_client(ip_address_input.text, int(port_input.text))
+	multiplayer.multiplayer_peer = peer
+	net_id_label.text = "Client Net Id : %s" % multiplayer.get_unique_id()
+
+func _on_host_as_player_pressed():
+	create_server()
+	create_player(1)
+
+# whenever a client connects spawn a new player
+# this will only ever be called on the server
+func handle_client_connected(id : int):
+	create_player(id)
+
 # whenever a client disconnects remove that player
+# this will only ever be called on the server
 func handle_client_disconnected(id : int):
 	if multiplayer.is_server():
 		for player in players:
