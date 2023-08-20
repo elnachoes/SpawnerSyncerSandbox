b59697c5bbde98d723a1cbc8e4c1798e11824971
Corbin Martin <corbin.martin@protonmail.com>
2023-08-20 03:02:19 -0700

    added a headless server mode

--- game.gd
+++ game.gd
@@ -13,21 +13,28 @@ var player_scene = load("res://player.tscn")
 
 var players = []
 
-# create server
-func _on_host_button_pressed():
+func _ready():
+	if OS.get_cmdline_args().find("server") != -1:
+		create_server()
+
+func create_server():
 	var peer = ENetMultiplayerPeer.new()
 	peer.create_server(PORT, MAX_CLIENTS)
 	multiplayer.multiplayer_peer = peer
 	multiplayer.peer_connected.connect(handle_client_connected)
 	multiplayer.peer_disconnected.connect(handle_client_disconnected)
-	net_id_label.text = "Net Id : %s" % multiplayer.get_unique_id()
+	net_id_label.text = "Server"
+
+# create server
+func _on_host_button_pressed():
+	create_server();
 
 # create client
 func _on_connect_button_pressed():
 	var peer = ENetMultiplayerPeer.new()
 	peer.create_client(IP_ADDRESS, PORT)
 	multiplayer.multiplayer_peer = peer
-	net_id_label.text = "Net Id : %s" % multiplayer.get_unique_id()
+	net_id_label.text = "Client Net Id : %s" % multiplayer.get_unique_id()
 
 # whenever a client connects spawn a new player
 func handle_client_connected(id : int):
