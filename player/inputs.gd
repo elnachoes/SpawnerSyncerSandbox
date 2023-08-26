c98703e78a6963b064e4ec000e4546437221fff5
Aidunlin <aidunlin@gmail.com>
2023-08-26 15:01:00 -0700

    add client-side prediction and server reconciliation

--- player/inputs.gd
+++ player/inputs.gd
@@ -2,8 +2,24 @@ class_name PlayerInputs
 extends Node
 
 @export var direction := Vector2()
+@export var sync_number := 0
 
+var history: Array[InputRecord] = []
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	if is_multiplayer_authority():
 		direction = Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down"))
+		if not multiplayer.is_server():
+			sync_number += 1
+			history.append(InputRecord.new(direction, sync_number, delta))
+
+
+class InputRecord:
+	var direction: Vector2
+	var sync_number: int
+	var delta: float
+	
+	func _init(init_direction: Vector2, init_sync_number: int, init_delta: float):
+		self.direction = init_direction
+		self.sync_number = init_sync_number
+		self.delta = init_delta
--- player/player.gd
+++ player/player.gd
@@ -2,8 +2,11 @@ class_name Player
 extends CharacterBody2D
 
 const SPEED = 300.0
+const position_lerp = 0.5
 
 @export var input_peer_id := 1
+@export var synced_position := position
+@export var sync_number := 0
 
 @onready var label := $Label as Label
 @onready var inputs := $Inputs as PlayerInputs
@@ -21,3 +24,22 @@ func _physics_process(_delta):
 	if multiplayer.is_server():
 		velocity = inputs.direction.normalized() * SPEED
 		move_and_slide()
+		
+		synced_position = position
+		sync_number = inputs.sync_number
+	else:
+		if multiplayer.get_unique_id() == input_peer_id:
+			synced_position = predict_and_reconcile(synced_position)
+		
+		position = position.lerp(synced_position, position_lerp)
+
+
+func predict_and_reconcile(pos: Vector2) -> Vector2:
+	inputs.history = inputs.history.filter(func(input: PlayerInputs.InputRecord):
+		return input.sync_number > sync_number
+	)
+	
+	var new_position = pos
+	for input in inputs.history:
+		new_position += input.direction.normalized() * SPEED * input.delta
+	return new_position
